f32bd908c92a179cfce195be557c4ef69241a049
Russ <forxxserious@gmail.com>
2017-04-05 20:57:25 +0000

    This is as far as we got in class.

--- thePage/models.py
+++ thePage/models.py
@@ -7,3 +7,12 @@ def aNumber():
     sTime = time.strftime("%S")
     iNum = int(sTime)
     return str(iNum * 3)
+    
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
